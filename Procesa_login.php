8c43ac45f6ae4fb7399f18a37a6309bafe7093c2
Francisco Vargas <franv2004@gmail.com>
2024-04-29 22:15:15 +0200

    Login
    Login 1 borrado y reemplazado con Login 2, ahora Login es lo que contenía Login2. BD cambiada y ya definitiva, en teoría

--- Procesa_login.php
+++ Procesa_login.php
@@ -1,9 +1,9 @@
 <?php
     
-if(isset($_POST["email"]) && isset($_POST["clave"])) {
+if(isset($_POST["email"]) && isset($_POST["passwd"])) {
     $bd = mysqli_connect("localhost", "root", "", "varlud");
     $email = $_POST["email"];
-    $passwd = $_POST["clave"];
+    $passwd = $_POST["passwd"];
     $query = mysqli_query($bd, "SELECT * FROM login WHERE correo='$email'");
 
     if(mysqli_num_rows($query) > 0) {
@@ -21,7 +21,7 @@ if(isset($_POST["email"]) && isset($_POST["clave"])) {
             foreach ($query2 as $dato2) {
                 $_SESSION["DNI"] = $dato2["DNI"];
             }
-            header("Location: Pagina_principal.php");
+            header("Location: pagina_principal.php");
         }else {
             header("Location: Login.php");
         }
--- login.php
+++ login.php
@@ -1,45 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="img/logo.png">
-	<title>Login</title>
-	<link rel="stylesheet" href="estilos.css">
+    <meta charset="UTF-8">
+    <title>VarLud Analytics</title>
 </head>
-<body bgcolor="lightblue">
-	<a href="Pagina_principal.php" ><img src="img/logo_tracers.png" width="150" style="text-align: center;"></a>
-	<marquee behaviour="scroll" direction="right"><p>PRUEBA</p></marquee>
-<?php
-session_start();
-    if(!isset($_SESSION["usuario"])) {
-?>
-	<div id="Inicia">
-	<form action="Procesa_login.php" method="POST">
-		Email: <input type="email" name="email"><br>
-		Contraseña: <input type="password" name="clave"><br>
-		<input type="submit">
-	</form>
-</div> 
-<?php
-	}else{
-		$login=$_SESSION["usuario"];
-		$dni=$_SESSION["DNI"];
-?>
-<div id="Iniciado">
-	<form action="Logout.php">
-		<?php echo "Sesión iniciada como $login <br>"; ?>
-		<input type="submit" value="Cerrar Sesión">
+<body>
+    <h2>Iniciar sesión</h2>
+    <form action="Procesa_login.php" method="POST">
+        <div>Correo:
+            <input type="text" name="email" required><br>
+            Contraseña:
+            <input type="password" name="passwd" required></div><br>
+            <input type="submit" value="Iniciar sesión">
+    </form>
+    <?php
 
-	</form>
-</div>
-<?php
-	}
-?>
-<div>
-	<form action="Registro.php">
-		<input type="submit" value="Registrarse">
-</div>
-<a href="Recordar.php">¿Has olvidado tu constraseña?</a>
+    ?>
 </body>
 </html>
