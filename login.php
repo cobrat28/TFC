573e6de3c0aab34af3712b46cffd3ea916593044
cobrat28 <121258541+cobrat28@users.noreply.github.com>
2024-05-01 12:55:51 +0200

    Organizacion

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>VarLud Analytics</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="CSS\estilos.css">
 </head>
 <body class="body_print">
     <form action="Procesa_login.php" method="POST" class="form">
