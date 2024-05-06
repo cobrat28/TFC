50632d940062392d21bb8fac95fb7483753fa077
cobrat28 <121258541+cobrat28@users.noreply.github.com>
2024-05-06 19:33:41 +0200

    Imágenes

--- Pagina_principal.php
+++ Pagina_principal.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
-    <body>
+    <body class="fondo" >
 
         <nav class="top">
             <div class="uno"><img class="uno_img" src="Imagenes\Logo_solo.jpg" alt="Aqui hay una imagen" height="50%"></div>
@@ -18,7 +18,7 @@
             <div class="cuatro"><p><a href="perfil.php">Mi Perfil</a></p></div>
         </nav>
 
-        <article class="fondo" style="display: flex; flex-wrap: wrap; height: 100%; margin-top: 0%;">
+        <article style="display: flex; flex-wrap: wrap; height: 100%; margin-top: 0%;">
             <div id="izq" style="margin-left: 10% ; margin-top : 5%; height: 45%; width : 20%; background-color: yellow; border-radius: 25px;"><p>prueba</p></div>
             <div id="cen" style="margin-left: 10% ; margin-top : 15%; height: 45%; width : 20%; background-color: lightgreen;border-radius: 25px;"><p>prueba</p></div>
             <div id="der" style="margin-left: 10% ; margin-top : 5%; height: 45%; width : 20%; background-color: #e96a6a;border-radius: 25px;"><p>prueba</p></div>
