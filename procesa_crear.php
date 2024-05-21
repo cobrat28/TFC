ae6ef92ddec3402de4d28eecf50043f00f547ce6
Francisco Vargas <franv2004@gmail.com>
2024-05-22 00:50:39 +0200

    Funciona todo

--- procesa_crear.php
+++ procesa_crear.php
@@ -1,79 +1,35 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>VarLud Analytics</title>
-    <link rel="icon" type="image/jpg" href="Imagenes/Logo_solo.png">
-    <link rel="stylesheet" href="CSS\estilos.css">
-</head>
-<body class="">
-<script type="text/Javascript" src="javascript.js"></script>
 <?php
 session_start();
-if (isset($_SESSION["admin"])) {
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if(isset($_SESSION["admin"])){
+    if($_SERVER["REQUEST_METHOD"]=="GET"){
         $preg = $_SESSION["preg"];
         $bd = mysqli_connect("localhost", "root", "", "varlud");
         $id = $_SESSION["ID_encuesta"];
-?>
-        <form action="" method="POST">
-            <?php
-            $sacar = mysqli_query($bd, "SELECT MIN(ID_pregunta) as 'minimo', MAX(ID_pregunta) as 'maximo' FROM preguntas where ID_encuesta = $id");
-            $min = 0;
-            $max = 0;
-            foreach ($sacar as $data) {
-                $min = $data["minimo"];
-                $max = $data["maximo"];
-            }
-            foreach (range($min, $max) as $número) {
-                echo $número;
-            }
-            $h = 0;
-            for ($i = $min; $i <= $max; $i++) {
+        for($i=0; $i < $preg; $i++){
+            //revisar action y method
             ?>
-                Pregunta: <input type="text" name="<?php echo 'txt' . $h; ?>"><br>
+            <form action="procesa_crear_dos.php" method = "GET">
+            Pregunta: <input type="text" name="<?php echo 'txt' . $i; ?>"><br>
                 Tipo de opciones:
-                <select id="<?php echo $h; ?>" name="<?php echo 'op' . $h; ?>" onchange="generar_opcion(this.value, <?php echo $i ?>, <?php echo $h ?>)">
+                <select id="<?php echo $h; ?>" name="<?php echo 'op' . $i; ?>">
                     <option id="texto" value="texto">Caja de texto</option>
                     <option id="radio" value="radio">Botones radio</option>
                     <option id="check" value="check">Botones check</option>
                     <option id="select" value="select">Desplegable</option>
                 </select><br>
-                <div id="$h"></div>
+            Cantidad de opciones (si procede): <input type="number" name="<?php echo 'cant_op' . $i; ?>"><br>
             <?php
-                $h++;
-            }
-            ?>
-            <input type="hidden" name="cant" value="<?php echo $h; ?>">
-            <input type="submit" value="Siguiente">
-        </form>
-            <div id=insert></div>
-<?php
-    } else {
-       /* $bd = mysqli_connect("localhost", "root", "", "varlud");
-        $id = $_SESSION["ID_encuesta"];
-        $preg = $_SESSION["preg"];
-        $sql = "INSERT INTO preguntas VALUES ";
-        for ($i = 1; $i <= $preg; $i++) {
-            $txt = $_POST["txt" . $i];
-            $op = $_POST["op" . $i];
-
-            $values = "(DEFAULT, '$id', '$op', '$txt')";
-            if ($i != $preg) {
-                $values = $values . ", ";
-            }
-            $sql = $sql . $values;
-
-            //echo("<script>console.log('ELSE PHP: " . $txt . " , " . $op . " , " . $i ."');</script>");
         }
-        $sql = $sql . ";";
-        //echo("<script>console.log('BD: " . $sql ."');</script>");
-        mysqli_query($bd, $sql);
-        //echo "<div id=insert>";*/
+        ?>
+        <input type="submit" value="Siguiente">
+    </form>
+    <?php
+    }else{
+        //$bd = mysqli_connect("localhost", "root", "", "varlud");
+        //$id = $_SESSION["ID_encuesta"];
+        //mysqli_query($bd, "INSERT INTO preguntas VALUES (DEFAULT, '$id', '$op', '$txt')");
     }
-} else {
+
+}else{
     header("Location: perfil.php");
 }
-?>
-</body>
-</html>
---
+++ procesa_crear_dos.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+if (isset($_SESSION["admin"])) {
+    $bd = mysqli_connect("localhost", "root", "", "varlud");
+    $id_enc = $_SESSION["ID_encuesta"];
+    //where max(ID_pregunta)
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $preg = $_SESSION["preg"];
+        $txt = 0;
+        $op = 0;
+        $id = $_SESSION["ID_encuesta"];
+        for ($i = 0; $i < $preg; $i++) {
+            $txt = $_GET["txt" . $i];
+            $op = $_GET["op" . $i];
+            $cant = $_GET["cant_op" . $i];
+            echo "$txt <br>";
+            echo "$op <br>";
+            echo "$cant <br>";
+            echo "$id <br>";
+            $pru = "INSERT INTO preguntas VALUES (DEFAULT, $id, '$op', '$txt')";
+            echo $pru;
+            mysqli_query($bd, $pru);
+            //hacer if para cazar los cant de text
+?>
+            <form action="" method="POST">
+                <p><?php echo $txt ?></p>
+                <input type="hidden" name="<?php echo 'preg' . $i; ?>" value="<?php echo $txt ?>">
+                <input type="hidden" name="<?php echo 'cant' . $i; ?>" value="<?php echo $cant ?>">
+                <?php
+                if($op != 'texto'){
+                for ($j = 0; $j < $cant; $j++) {
+                    $txt_sin = str_replace(' ', '', trim($txt . $j));
+                    echo "<br>";
+                    echo $txt . $j;
+                    echo "<br>";
+                    echo $txt_sin;
+                }
+                ?>
+                    <input type='text' name="<?php echo $txt_sin; ?>">
+                    <br>
+            <?php
+                }
+            } ?>
+            <input type="submit" value="Siguiente">
+            </form>
+    <?php
+    } else {
+        //nombrar el numero de preguntas de cada una, para poder hacer el bucle
+        $num_preg = $_SESSION["preg"];
+        for ($i = 0; $i < $num_preg; $i++) {
+            $preg = $_POST["preg" . $i];
+            //Solo llegan cuando no hay espacios de por medio, podemos porbar a crear una variable con el contenido de la pregunta sin espacios
+            $preg_t = str_replace(' ', '', trim($preg));
+            $cant = $_POST["cant" . $i];
+            //añadir un IF para las que no tengan opciones (texto)
+
+            if ($cant != 0) {
+                echo $preg_t . "<br>";
+                for ($j = 0; $j < $cant; $j++) {
+                    $txt = $_POST[$preg_t . $j];
+                    echo $txt;
+                    $query = mysqli_query($bd, "SELECT * FROM preguntas WHERE ID_encuesta = $id_enc AND texto = '$preg'");
+                    foreach ($query as $data) {
+                        $id = $data["ID_pregunta"];
+                        $tipo = $data["tipo"];
+                    }
+                    echo "<br>";
+                    echo $id;
+                    echo "<br>";
+                    echo $tipo;
+                    echo "<br>";
+                    if ($tipo == 'rad') {
+                        $tabla = 'op_radio';
+                    } elseif ($tipo == 'che') {
+                        $tabla = 'op_check';
+                    } elseif ($tipo == 'sel') {
+                        $tabla = 'op_select';
+                    } else {
+                        break;
+                    }
+                    echo $tabla;
+                    echo "<br>";
+                    $query2 = "INSERT INTO $tabla VALUES (DEFAULT, $id, '$txt')";
+                    echo $query2;
+
+                    mysqli_query($bd, $query2);
+                }
+            } else {
+                echo "No has introducido ningún valor";
+            }
+            echo $preg . "<br>";
+        }
+        //for(){
+
+        //}
+    }
+}
