8ca43c5ad5796c0eb5d94fee22aed43da7b0dc07
Francisco Vargas <franv2004@gmail.com>
2024-05-22 18:32:27 +0200

    antes del señor roca
    Ya no hay comentarios, todo furula, he puesto un botón al final. Podemos poner echo's con los datos a modo de resumen.

--- procesa_crear_dos.php
+++ procesa_crear_dos.php
@@ -13,12 +13,13 @@ if (isset($_SESSION["admin"])) {
             $txt = $_GET["txt" . $i];
             $op = $_GET["op" . $i];
             $cant = $_GET["cant_op" . $i];
+            /*
             echo "$txt <br>";
             echo "$op <br>";
             echo "$cant <br>";
-            echo "$id <br>";
+            echo "$id <br>";*/
             $pru = "INSERT INTO preguntas VALUES (DEFAULT, $id, '$op', '$txt')";
-            echo $pru;
+            //echo $pru;
             mysqli_query($bd, $pru);
             //hacer if para cazar los cant de text
 ?>
@@ -27,26 +28,30 @@ if (isset($_SESSION["admin"])) {
                 <input type="hidden" name="<?php echo 'preg' . $i; ?>" value="<?php echo $txt ?>">
                 <input type="hidden" name="<?php echo 'cant' . $i; ?>" value="<?php echo $cant ?>">
                 <?php
-                if($op != 'texto'){
-                for ($j = 0; $j < $cant; $j++) {
-                    $txt_sin = str_replace(' ', '', trim($txt . $j));
-                    echo "<br>";
-                    echo $txt . $j;
-                    echo "<br>";
-                    echo $txt_sin;
-                }
+                if ($op != 'texto') {
+                    for ($j = 0; $j < $cant; $j++) {
+                        $txt_sin = str_replace(' ', '', trim($txt . $j));
+                        /*
+                        echo "<br>";
+                        echo $txt . $j;
+                        echo "<br>";
+                        echo $txt_sin;
+                        */
+
                 ?>
-                    <input type='text' name="<?php echo $txt_sin; ?>">
-                    <br>
+                        <input type='text' name="<?php echo $txt_sin; ?>">
+                        <br>
             <?php
+                    }
                 }
             } ?>
             <input type="submit" value="Siguiente">
             </form>
-    <?php
+        <?php
     } else {
         //nombrar el numero de preguntas de cada una, para poder hacer el bucle
         $num_preg = $_SESSION["preg"];
+        echo "Encuesta creada con éxito, puede verla en el apartado de Encuestas en el menú principal";
         for ($i = 0; $i < $num_preg; $i++) {
             $preg = $_POST["preg" . $i];
             //Solo llegan cuando no hay espacios de por medio, podemos porbar a crear una variable con el contenido de la pregunta sin espacios
@@ -55,20 +60,22 @@ if (isset($_SESSION["admin"])) {
             //añadir un IF para las que no tengan opciones (texto)
 
             if ($cant != 0) {
-                echo $preg_t . "<br>";
+                //echo $preg_t . "<br>";
                 for ($j = 0; $j < $cant; $j++) {
                     $txt = $_POST[$preg_t . $j];
-                    echo $txt;
+                    //echo $txt;
                     $query = mysqli_query($bd, "SELECT * FROM preguntas WHERE ID_encuesta = $id_enc AND texto = '$preg'");
                     foreach ($query as $data) {
                         $id = $data["ID_pregunta"];
                         $tipo = $data["tipo"];
                     }
+                    /*
                     echo "<br>";
                     echo $id;
                     echo "<br>";
                     echo $tipo;
                     echo "<br>";
+                    */
                     if ($tipo == 'rad') {
                         $tabla = 'op_radio';
                     } elseif ($tipo == 'che') {
@@ -78,20 +85,22 @@ if (isset($_SESSION["admin"])) {
                     } else {
                         break;
                     }
-                    echo $tabla;
-                    echo "<br>";
+                    //echo $tabla;
+                    //echo "<br>";
                     $query2 = "INSERT INTO $tabla VALUES (DEFAULT, $id, '$txt')";
-                    echo $query2;
+                    //echo $query2;
 
                     mysqli_query($bd, $query2);
                 }
             } else {
                 echo "No has introducido ningún valor";
             }
-            echo $preg . "<br>";
+            //echo $preg . "<br>";
         }
-        //for(){
-
-        //}
+        ?>
+            <form action="Pagina_principal.php" method="POST">
+                <input type="submit" value="Volver a la página principal.">
+            </form>
+    <?php
     }
 }
