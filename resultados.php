21072aafbb9a632ed595a575a0294d4d2def550f
cobrat28 <121258541+cobrat28@users.noreply.github.com>
2024-05-31 22:19:53 +0200

    correciones

--- resultados.php
+++ resultados.php
@@ -13,16 +13,12 @@ if (isset($_SESSION["admin"])) {
     $cif = $_SESSION["CIF"];
     $bd=mysqli_connect("localhost", "root","", "varlud");
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        echo "<form action=perfil.php>";
-        echo "<input type='submit' value='Volver'>";
-        echo "</form>";
-        echo "";
-        echo "<h1 class=''> Aquí tienes tus encuestas, revisa los resultados </h1>";
+        echo "<h1 class='form3'> Aquí tienes tus encuestas, revisa los resultados </h1>";
         $query = mysqli_query($bd, "SELECT * FROM encuestas WHERE CIF='$cif'");
         foreach ($query as $data) {
             $id = $data["ID_encuesta"];
             $nom = $data["nombre"];
-            echo "<div class=''>";
+            echo "<div class='form11'>";
             echo "<form action='' method='POST'>";
             echo "<h1>" . $nom . "</h1>";
             echo "<h2>" . $id . "</h2>";
@@ -33,26 +29,28 @@ if (isset($_SESSION["admin"])) {
             echo "</div>";
         }
         echo "";
-        echo "<form action=perfil.php>";
+        echo "<form action=perfil.php style='height: 100px ; margin-top: 50px'>";
         echo "<input type='submit' value='Volver'>";
         echo "</form>";
     } else {
         $id_enc = $_POST["id"];
         $nom = $_POST["nom"];
-        echo "<h1>" . $nom . "</h1>";
+        echo "<h1 class='form3'>" . $nom . "</h1>";
         $query = mysqli_query($bd, "SELECT * FROM preguntas WHERE ID_encuesta=$id_enc");
         foreach ($query as $data) {
             $id_preg = $data["ID_pregunta"];
             $preg = $data["texto"];
-            echo "<h2>" . $preg . "</h2>";
+            echo "<div class='form12'><h2>" . $preg . "</h2>";
             $query2 = mysqli_query($bd, "SELECT * FROM respuestas WHERE ID_pregunta=$id_preg");
             foreach ($query2 as $data2) {
                 $res = $data2["respuesta"];
                 echo "$res <br>";
+                
             }
+            echo "</div>";
         }
         echo "";
-        echo "<form action='' method='GET'>";
+        echo "<form action='' method='GET' style='height: 100px ; margin-top: 50px'>";
         echo "<input type='submit' value='Volver'>";
         echo "</form>";
     }
