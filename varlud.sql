3ad703290ab83c6e1576d239668183deb8e15db5
Francisco Vargas <franv2004@gmail.com>
2024-06-04 19:10:45 +0200

    Update varlud.sql

--- varlud.sql
+++ varlud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2024 a las 19:09:38
+-- Tiempo de generación: 04-06-2024 a las 19:09:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -56,11 +56,7 @@ CREATE TABLE `encuestas` (
 --
 
 INSERT INTO `encuestas` (`ID_encuesta`, `CIF`, `nombre`) VALUES
-(141, 'Q87654321', 'Gestión de redes'),
-(142, 'Q87654321', 'prueba'),
-(143, 'Q87654321', 'Pito doble'),
-(144, 'Q87654321', 'Pito triple'),
-(145, 'Q87654321', 'Prueba textos');
+(141, 'Q87654321', 'Gestión de redes');
 
 -- --------------------------------------------------------
 
@@ -78,7 +74,7 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`correo`, `contraseña`) VALUES
-('miguel@varlud.com', '$2y$10$9RDnBgRMFB/19yHLmpsK1uf8BJJAph.uBn415PnOk9rrcXS2b4Tby'),
+('miguel@varlud.com', '$2y$10$jt8XS9CnMyGolyZT4AD55ucRsizRSWjo2frxV9msX3pUE3HEnmehm'),
 ('prueba@gmail.com', '$2y$10$vXuoZGngXP9w5RTa3Iba8e0oQN6ATgJnMiYY9Z0zcaMvtIozOdO7a');
 
 -- --------------------------------------------------------
@@ -109,9 +105,7 @@ INSERT INTO `op_check` (`ID_op_chk`, `ID_pregunta`, `valor`) VALUES
 (21, 351, 'FireWall.'),
 (22, 351, 'Puntos de acceso inalámbricos (AP)'),
 (23, 351, 'Servidores de seguridad.'),
-(24, 351, 'Dispositivos de almacenamiento en red (NAS)'),
-(37, 379, 'Ketchup'),
-(38, 379, 'Mostaza');
+(24, 351, 'Dispositivos de almacenamiento en red (NAS)');
 
 -- --------------------------------------------------------
 
@@ -144,11 +138,7 @@ INSERT INTO `op_radio` (`ID_op_rad`, `ID_pregunta`, `valor`) VALUES
 (76, 354, 'Costo de los servicios'),
 (77, 354, 'Nivel de soporte técnico ofrecido'),
 (78, 354, 'Gama de servicios y funcionalidades disponibles.'),
-(79, 354, 'Flexibilidad para adaptarse a las necesidades específicas de la empresa.'),
-(95, 377, 'Si '),
-(96, 377, 'No'),
-(97, 380, 'SOY VEGANO'),
-(98, 380, 'Nah flaco, dame un chuletón');
+(79, 354, 'Flexibilidad para adaptarse a las necesidades específicas de la empresa.');
 
 -- --------------------------------------------------------
 
@@ -176,10 +166,7 @@ INSERT INTO `op_select` (`ID_op_sel`, `ID_pregunta`, `valor`) VALUES
 (21, 352, 'Importante'),
 (22, 352, 'Neutral'),
 (23, 352, 'Poco imporante'),
-(24, 352, 'Me la pela'),
-(35, 382, 'Real'),
-(36, 382, 'Madrid'),
-(37, 382, 'CF');
+(24, 352, 'Me la pela');
 
 -- --------------------------------------------------------
 
@@ -208,20 +195,7 @@ INSERT INTO `preguntas` (`ID_pregunta`, `ID_encuesta`, `tipo`, `texto`) VALUES
 (352, 141, 'sel', '¿Qué nivel de importancia le das a la monitorización en tiempo real de la red de tu empresa?'),
 (353, 141, 'tex', '¿Cuáles son los principales desafíos que enfrentas actualmente en la gestión de redes de tu empresa?'),
 (354, 141, 'rad', '¿Cuál es el principal criterio que consideras al seleccionar un proveedor de servicios de gestión de red?'),
-(355, 141, 'tex', '¿Qué mejoras o actualizaciones te gustaria ver en los servicios de gestión de redes que actualmente utilices?'),
-(376, 142, 'tex', 'Prueba 1'),
-(377, 142, 'rad', 'Prueba 2'),
-(378, 143, 'tex', 'Salchipapa'),
-(379, 143, 'che', 'Perrito'),
-(380, 143, 'rad', 'SOS VEGANO?'),
-(381, 143, 'tex', 'Chorrada'),
-(382, 143, 'sel', 'De que equipo sos?'),
-(385, 144, 'tex', 'Fedelobo'),
-(386, 144, 'tex', 'Hola chata como estás'),
-(387, 145, 'tex', 'Prueba texto 1'),
-(388, 145, 'tex', 'Prueba texto 2'),
-(389, 145, 'tex', 'Prueba texto 1'),
-(390, 145, 'tex', 'Prueba texto 2');
+(355, 141, 'tex', '¿Qué mejoras o actualizaciones te gustaria ver en los servicios de gestión de redes que actualmente utilices?');
 
 -- --------------------------------------------------------
 
@@ -251,15 +225,7 @@ INSERT INTO `respuestas` (`ID_respuesta`, `ID_pregunta`, `ID_encuesta`, `DNI`, `
 (50, 352, 141, '12345678Q', 'Me la pela'),
 (51, 353, 141, '12345678Q', 'pito doble'),
 (52, 354, 141, '12345678Q', 'Costo de los servicios'),
-(53, 355, 141, '12345678Q', 'Pito triple'),
-(54, 376, 142, '12345678Q', 'Pito'),
-(55, 377, 142, '12345678Q', 'Si '),
-(56, 387, 145, '12345678Q', 'Pito'),
-(57, 388, 145, '12345678Q', 'Pito doble'),
-(58, 389, 145, '12345678Q', 'Pito triple'),
-(59, 390, 145, '12345678Q', 'Pito meteórico'),
-(60, 376, 142, '12345678Q', 'Pito'),
-(61, 377, 142, '12345678Q', 'No');
+(53, 355, 141, '12345678Q', 'Pito triple');
 
 -- --------------------------------------------------------
 
@@ -282,7 +248,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`nombre`, `apellidos`, `fecha_nac`, `correo`, `DNI`, `CIF`, `admin`) VALUES
-('Miguel Ángel', 'Ludeña', '2000-06-15', 'miguel@varlud.com', '12345678F', 'Q87654321', 1),
+('Miguel Ángel', 'Ludeña', '2000-06-15', 'miguel@varlud.com', '12345678L', 'Q87654321', 1),
 ('Francisco', 'Vargas', '2024-04-01', 'prueba@gmail.com', '12345678Q', 'Q87654321', 1);
 
 --
@@ -361,7 +327,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `encuestas`
 --
 ALTER TABLE `encuestas`
-  MODIFY `ID_encuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+  MODIFY `ID_encuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
 
 --
 -- AUTO_INCREMENT de la tabla `op_check`
@@ -373,7 +339,7 @@ ALTER TABLE `op_check`
 -- AUTO_INCREMENT de la tabla `op_radio`
 --
 ALTER TABLE `op_radio`
-  MODIFY `ID_op_rad` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+  MODIFY `ID_op_rad` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT de la tabla `op_select`
@@ -385,7 +351,7 @@ ALTER TABLE `op_select`
 -- AUTO_INCREMENT de la tabla `preguntas`
 --
 ALTER TABLE `preguntas`
-  MODIFY `ID_pregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=391;
+  MODIFY `ID_pregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=393;
 
 --
 -- AUTO_INCREMENT de la tabla `respuestas`
